79fdcd6fc991b720f941c639f3dfd3fb680b29ca
Mateusz Janota <m.janota@lynth.io>
2021-03-24 11:24:09 +0100

    Fixing CSP

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ function getCsp(nonce) {
   csp += `form-action 'self';`;
   csp += `default-src 'self';`;
   csp += `font-src 'self' script.hotjar.com;`;
-  csp += `connect-src 'self' vitals.vercel-insights.com www.google-analytics.com;`;
+  csp += `connect-src 'self' vitals.vercel-insights.com *.ingest.sentry.io www.google-analytics.com;`;
   csp += `script-src 'self' 'nonce-${nonce}' www.googletagmanager.com *.ingest.sentry.io www.google-analytics.com static.hotjar.com script.hotjar.com ${process.env.NODE_ENV === "production" ? "" : "'unsafe-eval'"} 'unsafe-inline';`;
   csp += `style-src 'self' 'unsafe-inline' data:;`;
   csp += `img-src 'self' www.google-analytics.com data: blob:;`;
